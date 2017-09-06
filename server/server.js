7ad3d0d33640f0ef62400c04d57559e59f49c3a9
Yuriy Cat <very.good.kot@gmail.com>
2017-09-06 07:18:47 +0300

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -37,11 +37,24 @@ app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    console.log(1);
     return res.status(404).send();
   }
   Todo.findById(id).then((todo) => {
     if(!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
       return res.status(404).send();
     }
     res.send({todo});
